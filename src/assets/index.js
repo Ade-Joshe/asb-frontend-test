ef35ff561189959b777999330c2b84a1c20cf212
Adegoke Joshua Oluwafemi <femijay9@gmail.com>
2022-05-21 23:15:18 +0100

    added and linked fonts and image assets

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import logo from './img/logo.png';
+import divisionIcon from './svg/division.svg';
+import homeIcon from "./svg/home.svg";
+import entries from "./svg/entries.svg";
+import search from "./svg/search.svg";
+import bell from "./svg/doorbell.svg";
+import user from "./svg/user.svg";
+import arrowDown from "./svg/arrow-down.svg";
+import circleDown from "./svg/circle-down.svg";
+import arrowRight from "./svg/arrow-right.svg";
+import info from "./svg/info.svg";
+import divisionSummary from "./svg/divisionSummary.svg";
+import moduleHistory from "./svg/moduleHistory.svg";
+import phone from "./svg/phone.svg";
+import email from "./svg/mail.svg";
+import location from "./svg/location.svg";
+import journal from "./svg/journal.svg";
+import fingerprint from "./svg/fingerprint.svg";
+import low from "./svg/low.svg";
+import mid from "./svg/mid.svg";
+import high from "./svg/high.svg";
+import ellipses from "./svg/ellipses.svg";
+
+export {
+    logo, divisionIcon, homeIcon, entries, search, circleDown,
+    bell, user, arrowDown, arrowRight, info, divisionSummary,
+    moduleHistory, phone, email, location, journal, fingerprint,
+    low, mid, high, ellipses
+};
